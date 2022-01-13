e493ec1fdc945dc130790de9b66e5cc1ffb45fc0
black3279 <30133279@naver.com>
2022-01-14 00:04:49 +0900

    220114

--- Inflearn/documents/boiler-plate/index.js
+++ Inflearn/documents/boiler-plate/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://black3279:TlrwvwunHIzY3Ngh@cluster0.tpbus.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
